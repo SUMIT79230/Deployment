7edef7e3ea313626d817607161023febba7e667f
Sumit <sumitkumar79230@gmail.com>
2024-04-26 17:10:13 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "Container1">
+        
+      </div>
+      <div className = "Container2">
+        <h1>Application Deployed Sucessfully !</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
